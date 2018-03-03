bc563d2b74dda0695bed22a12e32d8605da74a01
Arvind Madugolkar Vithal <arvind.madugolkarvitha@mavs.uta.edu>
2018-03-02 22:52:02 -0600

    acs

--- d3ex/app.py
+++ d3ex/app.py
@@ -3,17 +3,38 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
-from flask import Flask
+from flask import Flask, render_template, Response,make_response
+ 
+
+import random
+import json
+import pandas
+import numpy as np
+ 
+df = pandas.DataFrame({
+    "x" : [11,28,388,400,420],
+    "y" : np.random.rand(5)
+})
+ 
+d = [
+    dict([
+        (colname, row[i])
+        for i,colname in enumerate(df.columns)
+    ])
+    for row in df.values
+]
 app = Flask(__name__)
-
-# Make the WSGI interface available at the top level so wfastcgi can get it.
-wsgi_app = app.wsgi_app
-
-
-@app.route('/')
-def hello():
-    """Renders a sample page."""
-    return "Hello World!"
+ 
+@app.route('/streamdata')
+def event_stream():
+    make_response(d.to_json())
+ 
+@app.route('/stream')
+def show_basic():
+    x = random.randint(0,101)
+    y = random.randint(0,101)
+    #print json.dumps(d)
+    return render_template("redisd3.html",data=json.dumps(d))
 
 if __name__ == '__main__':
     import os
